b5ddfe008be0d493aaf8220c98b76ca5e23a9d42
Anastasia <nastya.nedoseicko@yandex.ru>
2020-03-07 17:51:36 +0300

    Ничего особенного в этом коммите тоже нет

---
+++ data/jobs.py
@@ -0,0 +1,16 @@
+import sqlalchemy
+from sqlalchemy import orm
+
+
+class Jobs(SqlAlchemyBase):
+    __tablename__ = 'jobs'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    team_leader = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
+    job = sqlalchemy.Column(sqlalchemy.String)
+    work_size = sqlalchemy.Column(sqlalchemy.Integer)
+    collaborators = orm.relation(sqlalchemy.String)
+    start_date = sqlalchemy.Column(sqlalchemy.DateTime)
+    end_date = sqlalchemy.Column(sqlalchemy.DateTime)
+    is_finished = sqlalchemy.Column(sqlalchemy.Boolean)
+    user = orm.relation('User')
